4a98b87daa3ec320846df1c852853b08fc8b11ea
Jeremias Iturriza <79233911+jeremiasiturriza@users.noreply.github.com>
2023-02-08 17:38:03 -0300

    Update twitter scraper.py

--- twitter scraper.py
+++ twitter scraper.py
@@ -3,7 +3,7 @@ import snscrape.modules.twitter as tw
 
 def tweets(hashtag, cant_tweets):
     '''
-    Dado un hashtag y una cantidad de tweets deseada
+    Dado un termino de busqueda y una cantidad de tweets deseada
     devuelve una lista con esos tweets
     '''
     scraper = tw.TwitterSearchScraper(hashtag)
@@ -24,7 +24,7 @@ def tweets(hashtag, cant_tweets):
 def to_DataFrame(lista, orden, export = True):
     '''
     Convierte la lista generada en un DataFrame
-    Se debe elegir una columna para que ordene:
+    Se debe elegir una columna para que sea ordenada:
     'Fecha', 'Likes', 'Retweets'
     Puede elegir exportarlo o bien utilizarlo como df
     '''
@@ -38,7 +38,7 @@ def to_DataFrame(lista, orden, export = True):
                         6 : 'URL'})
                             
     
-    df['Fecha'] = df['Fecha'].apply(lambda x: x.strftime("%d/%m/%y %r"))
+    df['Fecha'] = df['Fecha'].apply(lambda x: x.strftime("%d/%m/%y %r")) # Transforma el formato de fecha y hora a uno mas comodo para mi
     
     df = df.sort_values(orden, ascending = False)
     
@@ -47,7 +47,7 @@ def to_DataFrame(lista, orden, export = True):
     else:
         return df
 
-tweet = get_tweets('argentina campeon', 1000)
-df = to_DataFrame(tweet,'Likes', export=True)
+tweet = get_tweets('argentina campeon', 1000) # Test
+df = to_DataFrame(tweet,'Likes', export=True) # Test
 
 
